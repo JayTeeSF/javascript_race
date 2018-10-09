b2859f711ec6c0116c7b748155f5fd41463841bf
JayTeeSF <jonathan@his-service.net>
2018-10-09 16:42:42 -0700

    html & css templates; initial js; tbd: startBtn

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-//script
+const speedDash = document.querySelector(".speedDash");
+const scoreDash = document.querySelector(".scoreDash");
+const lifeDash = document.querySelector(".lifeDash");
+
+const container = document.getElementById("container");
+const btnStart = document.querySelector(".btnStart");
+let animationGame = requestAnimationFrame(playGame);
+
+let gamePlay = false;
+
+let keys = {
+    ArrowUp:false,
+    ArrowDown:false,
+    ArrowLeft:false,
+    ArrowRight:false
+}
+
+function playGame() {
+    if (gamePlay) {
+      console.log("Game in play...");
+    }
+    animationGame = requestAnimationFrame(playGame);        
+}
